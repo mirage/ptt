62e804c06b6d1d18635f6a2fb21b77ae3a577bcb
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-02-07 17:24:51 +0100

    Custom print of mailbox

--- bin/scan.ml
+++ bin/scan.ml
@@ -24,6 +24,43 @@ let pp_perm ppf stat =
     w_perm (stat.Unix.st_perm land 0x2  )
     x_perm (stat.Unix.st_perm land 0x1  )
 
+let pp_mailbox ppf mailbox =
+  let open Mrmime.Mailbox in
+  let pp_word ppf = function `Atom x -> Fmt.string ppf x | `String x -> Fmt.pf ppf "%S" x in
+  let pp_literal_domain ppf = function
+    | IPv4 x -> Fmt.pf ppf "[%a]" Ipaddr.V4.pp x
+    | IPv6 x -> Fmt.pf ppf "[IPv6:%a]" Ipaddr.V6.pp x
+    | Ext (ldh, v) -> Fmt.pf ppf "[%s:%s]" ldh v in
+  let pp_domain ppf = function
+    | `Domain lst -> Fmt.(list ~sep:(const string ".") string) ppf lst
+    | `Literal x -> Fmt.pf ppf "[%s]" x
+    | `Addr x -> pp_literal_domain ppf x in
+  let pp_local = Fmt.(list ~sep:(const string ".") pp_word) in
+  let pp_elt ppf = function
+    | `Dot -> Fmt.string ppf "."
+    | `Word x -> pp_word ppf x
+    | `Encoded { Mrmime.Encoded_word.data = Ok raw; _ } ->
+      (* Handle output, UTF-8, ISO-8859? *)
+      Fmt.string ppf raw
+    | `Encoded _ -> Fmt.string ppf "<?>" in
+  let pp_phrase = Fmt.(list ~sep:(const string " ") pp_elt) in
+  match mailbox.name, mailbox.domain with
+  | Some name, (domain, []) ->
+    Fmt.pf ppf "%a <%a@%a>" pp_phrase name pp_local mailbox.local pp_domain domain
+  | Some name, (domain, rest) ->
+    Fmt.pf ppf "%a <%a:%a@%a>"
+      pp_phrase name
+      Fmt.(list ~sep:(const string ",") (prefix (const string "@") pp_domain)) rest
+      pp_local mailbox.local
+      pp_domain domain
+  | None, (domain, []) ->
+    Fmt.pf ppf "<%a@%a>" pp_local mailbox.local pp_domain domain
+  | None, (domain, rest) ->
+    Fmt.pf ppf "<%a:%a@%a>"
+      Fmt.(list ~sep:(const string ",") (prefix (const string "@") pp_domain)) rest
+      pp_local mailbox.local
+      pp_domain domain
+
 let pp_user ppf stat =
   let user = Unix.getpwuid stat.Unix.st_uid in
   Fmt.string ppf user.Unix.pw_name
@@ -36,11 +73,8 @@ let pp_time =
   let pp = Fmt.(option ~none:(const string "#invalid-date") Ptime.pp) in
   Fmt.using Ptime.of_float_s pp
 
-let pp_size ppf stat =
-  Fmt.int ppf stat.Unix.st_size
-
-let pp_pad n pp =
-  Fmt.using (pad n <.> Fmt.strf "%a" pp) Fmt.string
+let pp_size ppf stat = Fmt.int ppf stat.Unix.st_size
+let pp_pad n pp = Fmt.using (pad n <.> Fmt.strf "%a" pp) Fmt.string
 
 let parse_header new_line ic =
   let open Angstrom.Buffered in
@@ -55,10 +89,8 @@ let parse_header new_line ic =
     | Fail (_, _, err) -> close_in ic ; Error (`Msg err) in
   go (parse Mrmime.Mail.header)
 
-type error = [ `Not_found | Rresult.R.msg ]
-
 let get_field
-  : type a. a Mrmime.Header.field -> Mrmime.Header.t -> ((a * Mrmime.Location.t) list, error) result
+  : type a. a Mrmime.Header.field -> Mrmime.Header.t -> ((a * Mrmime.Location.t) list, [ `Not_found | Rresult.R.msg ]) result
   = fun field header ->
     match Mrmime.Header.get field header with
     | [] -> Error `Not_found
@@ -76,10 +108,14 @@ let get_fields fields header =
       | Error e -> Error e in
   List.fold_left f (Ok []) fields
 
+let pp_value_of_field : type a. a Mrmime.Header.field -> a Fmt.t = function
+  | Mrmime.Header.From -> Fmt.(list ~sep:(const string ", ") pp_mailbox)
+  | field -> Mrmime.Header.pp_value_of_field field
+
 let pp_binding ppf (Mrmime.Header.B (field, v, _)) =
   Fmt.pf ppf "%a %a"
     (pp_pad 15 Fmt.(suffix (const string ":") (using Mrmime.Header.field_to_string string))) field
-    (Mrmime.Header.pp_value_of_field field) v
+    (pp_value_of_field field) v
 
 module Status = struct
   let ok = Fmt.const Fmt.string "[OK]" |> Fmt.styled `Green
