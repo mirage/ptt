daefde92d6d437ef14ef9ddf4ccf157dc20e84a5
Calascibetta Romain <romain.calascibetta@gmail.com>
2020-01-29 16:50:01 +0100

    Add Received field when we transmit an email

--- lib/relay.ml
+++ lib/relay.ml
@@ -11,7 +11,9 @@ module Make
   module Ke = Ke.Rke.Weighted
   module Md = Messaged.Make(Scheduler)(IO)
 
+  let return = IO.return
   let ( >>= ) = IO.bind
+  let ( >|= ) x f = x >>= fun x -> return (f x)
 
   let src = Logs.Src.create "ptt-relay"
   module Log = (val Logs.src_log src : Logs.LOG)
@@ -30,6 +32,7 @@ module Make
     { domain : [ `host ] Domain_name.t
     ; ipv4 : Ipaddr.V4.t
     ; tls : Tls.Config.server
+    ; zone : Mrmime.Date.Zone.t
     ; size : int64 }
 
   let info { info; _ } = info
@@ -75,16 +78,16 @@ module Make
     | Ok v -> f v
     | Error err -> IO.return (Error err)
 
-  let io_fold_list ~f a l =
+  let list_fold_left_s ~f a l =
     let rec go a = function
       | [] -> IO.return a
-      | x :: r -> f x a >>= fun a -> go a r in
+      | x :: r -> f a x >>= fun a -> go a r in
     go a l
 
   (* XXX(dinosaure): this function checks only if domains have a reachable Mail Exchange service. *)
   let recipients_are_reachable s w recipients =
     let open Colombe in
-    let fold { Dns.Mx.mail_exchange; Dns.Mx.preference; } m =
+    let fold m { Dns.Mx.mail_exchange; Dns.Mx.preference; } =
       s.resolver.gethostbyname w mail_exchange >>= function
       | Ok mx_ipaddr ->
         let mx_ipaddr = Ipaddr.V4 mx_ipaddr in (* TODO: [gethostbyname] should return a [Ipaddr.t]. *)
@@ -103,7 +106,7 @@ module Make
             s.resolver.getmxbyname w domain
             >>= function
             | Ok m ->
-              io_fold_list ~f:fold Mxs.empty (Dns.Rr_map.Mx_set.elements m) >>= fun s ->
+              list_fold_left_s ~f:fold Mxs.empty (Dns.Rr_map.Mx_set.elements m) >>= fun s ->
               go (s :: acc) r
             | Error (`Msg err) ->
               Log.warn (fun m -> m "Got an error while resolving %a: %s" Domain_name.pp domain err) ;
@@ -126,6 +129,72 @@ module Make
           Log.warn (fun m -> m "Got an error while resolving [%s:%s]: %s" ldh v err) ;
           go acc r in
     go [] recipients >>= (IO.return <.> List.for_all (fun m -> not (Mxs.is_empty m)))
+
+  let resolve_recipients s w relay_map recipients =
+    let domain = s.info.domain in
+    let dns = s.resolver in
+    let module Resolved = Map.Make(struct
+        type t = [ `Ipaddr of Ipaddr.t | `Domain of ([ `host ] Domain_name.t * Mxs.t) ]
+
+        let compare a b = match a, b with
+          | `Ipaddr a, `Ipaddr b -> Ipaddr.compare a b
+          | `Domain (_, mxs_a), `Domain (_, mxs_b) ->
+            let { Mxs.mx_ipaddr= a; _ } = Mxs.choose mxs_a in
+            let { Mxs.mx_ipaddr= b; _ } = Mxs.choose mxs_b in
+            Ipaddr.compare a b
+          | `Ipaddr a, `Domain (_, mxs) ->
+            let { Mxs.mx_ipaddr= b; _ } = Mxs.choose mxs in
+            Ipaddr.compare a b
+          | `Domain (_, mxs), `Ipaddr b ->
+            let { Mxs.mx_ipaddr= a; _ } = Mxs.choose mxs in
+            Ipaddr.compare a b
+      end) in
+    let postmaster = [ `Atom "Postmaster" ] in
+    let unresolved, resolved = Aggregate.aggregate_by_domains ~domain recipients in
+    let unresolved, resolved = Relay_map.expand relay_map unresolved resolved in
+    let fold resolved (domain, recipients) =
+      dns.getmxbyname w domain >>= function
+      | Error (`Msg err) ->
+        Log.err (fun m -> m "Domain %a does not have a Mail Exchange service: %s" Domain_name.pp domain err) ;
+        IO.return resolved
+      | Ok mxs ->
+        let fold mxs { Dns.Mx.mail_exchange; Dns.Mx.preference; } =
+          dns.gethostbyname w mail_exchange >>= function
+          | Ok mx_ipaddr ->
+            let mxs = Mxs.(add (v ~preference ~domain:mail_exchange (Ipaddr.V4 mx_ipaddr)) mxs) in
+            IO.return mxs
+          | Error (`Msg err) ->
+            Log.warn (fun m -> m "Impossible to reach the Mail Exchange service %a: %s" Domain_name.pp mail_exchange err ) ;
+            IO.return mxs in
+        list_fold_left_s ~f:fold Mxs.empty (Dns.Rr_map.Mx_set.elements mxs) >>= fun mxs ->
+        if Mxs.is_empty mxs
+        then ( Log.err (fun m -> m "Domain %a does not have a reachable Mail Exchange server." Domain_name.pp domain)
+             ; IO.return resolved )
+        else
+          let { Mxs.mx_ipaddr; _ } = Mxs.choose mxs in
+          match recipients with
+          | `All -> IO.return (Resolved.add (`Domain (domain, mxs)) `All resolved)
+          | `Local l0 ->
+            ( match Resolved.find_opt (`Ipaddr mx_ipaddr) resolved with
+              | None -> IO.return (Resolved.add (`Domain (domain, mxs)) (`Local l0) resolved)
+              | Some `All -> IO.return resolved
+              | Some (`Local l1) ->
+                let vs = List.sort_uniq (Emile.compare_local ~case_sensitive:true) (l0 @ l1) in
+                IO.return (Resolved.add (`Domain (domain, mxs)) (`Local vs) resolved)
+              | Some `Postmaster ->
+                IO.return (Resolved.add (`Domain (domain, mxs)) (`Local (postmaster :: l0)) resolved) )
+          | `Postmaster -> match Resolved.find_opt (`Ipaddr mx_ipaddr) resolved with
+            | Some `Postmaster | Some `All -> IO.return resolved
+            | Some (`Local l0) ->
+              if List.exists (Emile.equal_local ~case_sensitive:true postmaster) l0
+              then IO.return resolved
+              else IO.return (Resolved.add (`Domain (domain, mxs)) (`Local (postmaster :: l0)) resolved)
+            | None ->
+              IO.return (Resolved.add (`Domain (domain, mxs)) (`Local [ postmaster ]) resolved) in
+    let open Aggregate in
+    let resolved = By_ipaddr.fold (fun k v m -> Resolved.add (`Ipaddr k) v m) resolved Resolved.empty in
+    list_fold_left_s ~f:fold resolved (By_domain.bindings unresolved) >|= fun resolved ->
+    Resolved.bindings resolved
 
   let dot = Some (".\r\n", 0, 3)
 
