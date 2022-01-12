a41deec756265cb302d9b11b736c6d7964cac503
Romain Calascibetta <romain.calascibetta@gmail.com>
2022-01-12 15:09:22 +0100

    Delete pin-depends on irmin and require at least irmin.2.10.0

--- unikernel/submission/config.ml
+++ unikernel/submission/config.ml
@@ -228,17 +228,15 @@ let keys =
       ; abstract account_seed
       ; abstract cert_seed ]
 
-let pin_irmin = "git+https://github.com/dinosaure/irmin.git#65d5fc6ef7d6dd76d103559edee0ad6036a896af"
-
 let packages =
   [ package "randomconv"
   ; package "ptt" ~sublibs:[ "lipap" ]
   ; package "ptt" ~sublibs:[ "irmin" ]
-  ; package ~pin:pin_irmin "irmin-mirage-git"
-  ; package ~pin:pin_irmin "irmin-mirage"
-  ; package ~pin:pin_irmin "irmin-git"
-  ; package ~pin:pin_irmin "irmin"
-  ; package ~pin:pin_irmin "ppx_irmin"
+  ; package "irmin-mirage-git"
+  ; package "irmin-mirage"
+  ; package "irmin-git"
+  ; package "irmin" ~min:"2.10.0"
+  ; package "ppx_irmin"
   ; package "ptt"
   ; package "dns-mirage"
   ; package "paf" ~sublibs:[ "mirage" ]
